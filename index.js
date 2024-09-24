f1a14457b6fb899a877026859bf4055699bdbb1a
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2024-09-24 09:16:22 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var readlineSync = require('readline-sync');
+
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+
+// crush api
+// 
+// love her? -> propose her 
+// is valid to Marry? -> marry her
+// love life analysis
